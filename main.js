0460fee599c7cd17fe1dd782d32c13e8865b7f19
Subin <ryusb981211@gmail.com>
2024-07-03 16:57:35 +0900

    second commit

--- main.js
+++ main.js
@@ -8,38 +8,123 @@
 
 let addBtn = document.getElementById('addBtn');
 let taskInput = document.getElementById('taskInput');
-let checkBtn = document.getElementById('checkBtn');
-let deleteBtn = document.getElementById('deleteBtn');
+// let checkBtn = document.getElementById('checkBtn');
+// let deleteBtn = document.getElementById('deleteBtn');
+let taskTaps = document.querySelectorAll('.task-taps div'); // 배열로생성
 let taskList = [];
+let filterList = [];
+let mode = 'all';
 
 addBtn.addEventListener('click', add);
-checkBtn.addEventListener('click', check);
-deleteBtn.addEventListener('click', deleteList);
+// checkBtn.addEventListener('click', check);
+// deleteBtn.addEventListener('click', deleteList);
+for (let i = 1; i < taskTaps.length; i++) {
+  taskTaps[i].addEventListener('click', function (event) {
+    filter(event);
+  });
+}
 
 function add() {
   // 할일 목록에 추가하기
-  taskContent = taskInput.value;
-  taskList.push(taskContent);
+  console.log('clicked');
+  let task = {
+    id: randomIDGenerate(),
+    taskContent: taskInput.value,
+    isComplete: false,
+  };
+  // console.log(task);
+  taskList.push(task);
   render();
 }
 function render() {
   // 할 일 그려주기
   let resultHTML = '';
-  for (let i = 0; i < taskList.length; i++) {
-    resultHTML += `<div class="task">
-            <div>${taskList[i]}</div>
+  let list = [];
+
+  if (mode === 'all') {
+    list = taskList;
+  } else if (mode === 'notDone' || mode === 'done') {
+    list = filterList;
+  }
+  for (let i = 0; i < list.length; i++) {
+    if (list[i].isComplete == true) {
+      resultHTML += `<div class="task grey">
+      <div class="task-done">${list[i].taskContent}</div>
+      <div>
+        <button onclick="toggleComplete('${list[i].id}')"><i class="fa-solid fa-rotate-left"></i></button>
+        <button onclick="deleteTask('${list[i].id}')"><i class="fa-solid fa-trash" style="color: #ff1414;"></i></button>
+      </div>
+    </div>`;
+    } else {
+      resultHTML += `<div class="task">
+            <div>${list[i].taskContent}</div>
             <div>
-              <button id="checkBtn">Check</button>
-              <button id="deleteBtn">Delete</button>
+              <button onclick="toggleComplete('${list[i].id}')"><i class="fa-solid fa-check" style="color: #21cc1e;"></i></button>
+              <button onclick="deleteTask('${list[i].id}')"><i class="fa-solid fa-trash" style="color: #ff1414;"></i></button>
             </div>
           </div>`;
+    }
   }
 
   document.getElementById('taskBoard').innerHTML = resultHTML;
 }
-function check() {
-  // 밑줄 긋기
+function toggleComplete(id) {
+  console.log('id:', id);
+  for (let i = 0; i < taskList.length; i++) {
+    if (id == taskList[i].id) {
+      taskList[i].isComplete = !taskList[i].isComplete;
+
+      break;
+    }
+  }
+
+  filter();
 }
-function deleteList() {
+function deleteTask(id) {
   //할 일 목록에서 삭제
+  console.log('삭제하자');
+  for (let i = 0; i < taskList.length; i++) {
+    if (taskList[i].id == id) {
+      console.log('삭제');
+      taskList.splice(i, 1);
+      console.log(taskList);
+
+      break;
+    }
+  }
+
+  render();
 }
+function filter(event) {
+  if (event) {
+    mode = event.target.id;
+  }
+  filterList = [];
+  // console.log(event.target.id); //event.target;
+  // mode = event.target.id;
+  if (mode === 'all') {
+    render();
+  } else if (mode === 'notDone') {
+    console.log(mode);
+    for (let i = 0; i < taskList.length; i++) {
+      if (taskList[i].isComplete == false) {
+        filterList.push(taskList[i]);
+      }
+    }
+    console.log(filterList);
+    render();
+  } else if (mode === 'done') {
+    console.log(mode);
+    for (let i = 0; i < taskList.length; i++) {
+      if (taskList[i].isComplete == true) {
+        filterList.push(taskList[i]);
+      }
+    }
+    console.log(filterList);
+    render();
+  }
+}
+function randomIDGenerate() {
+  return '_' + Math.random().toString(36).substr(2, 9);
+}
+// render();
---
+++ test.js
@@ -0,0 +1,6 @@
+console.log(document.getElementById('test').innerHTML);
+console.log(document.getElementById('test').innerText);
+console.log(document.getElementById('test').textContent);
+
+// document.getElementById('test').innerHTML = `<hi>Noona</h1>`;
+document.getElementById('test').textContent = `<hi>Noona</h1>`;
